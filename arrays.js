25179b02c9ab83913b73f12fbee73afe3cbd763b
Laura Menegazzo Boeing <l.boeing@escola.pr.gov.br>
2026-05-07 11:33:02 -0300

    fim

---
+++ arrays.js
@@ -0,0 +1,14 @@
+const lojaA = [
+    {nome:"Samsung Galaxy S20", preco: 4000},
+    {nome:"iPhone 11", preco: 1500},
+
+];
+const lojaB = [
+    {nome:"Samsung Galaxy S20", preco: 3500},
+    {nome:"iPhone 11", preco: 2000},
+    {nome:"Xiaomi Mi 10", preco: 1000},
+    {nome:"OnePlus 8 Pro", preco: 1900},
+    {nome:"Google Pixel 4", preco: 800}
+];
+
+module.exports = {lojaA, lojaB};
---
+++ index.js
@@ -0,0 +1,46 @@
+const { lojaA, lojaB } = require('./arrays');
+
+
+function juntaListas(lista1, lista2) {
+    let listaFinal = [];
+    let posicaoAtualLista1 = 0
+    let posicaoAtualLista2 = 0
+    let atual = 0
+    //enquanto ainda tem elemento nas duas listas
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
+        let produtoAtualLista1 = lista1[posicaoAtualLista1];
+        let produtoAtualLista2 = lista2[posicaoAtualLista2];
+
+
+        if (produtoAtualLista1.preco < produtoAtualLista2.preco) {
+            listaFinal[atual] = produtoAtualLista1;
+            posicaoAtualLista1++;
+        } else {
+            listaFinal[atual] = produtoAtualLista2;
+            posicaoAtualLista2++;
+        }
+        atual++;
+    }
+    //adiciona o restante da lista 1(se sobrar)
+    while (posicaoAtualLista1 < lista1.length) {
+        listaFinal[atual] = lista1[posicaoAtualLista1];
+        posicaoAtualLista1++;
+        atual++;
+    }
+    //adiciona o restante da lista 2(se sobrar)
+    while (posicaoAtualLista2 < lista2.length) {
+        listaFinal[atual] = lista2[posicaoAtualLista2];
+        posicaoAtualLista2++;
+        atual++;
+    }
+
+
+
+
+    return listaFinal;
+
+
+}
+console.log(juntaListas(lojaA, lojaB));
+
+
